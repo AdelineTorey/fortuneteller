0b48b8b40278c89601afbc0015a9e20c68d27780
Adeline Torey <55276131+AdelineTorey@users.noreply.github.com>
2020-05-01 14:38:57 +0200

    Add files via upload

---
+++ R/fortuneteller.R
@@ -0,0 +1,7 @@
+#' Digital Fortune Teller
+#' Contains one function which simulates the fortune teller origami game
+#' @docType package
+#' @author Adeline Torey \email(adeline.torey@gmail.com)
+#'
+#' @name fortuneteller
+NULL
